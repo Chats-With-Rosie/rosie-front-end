4601e10c8dd5a4b6c80c107cf98222b8d78b1a07
Cameron Gilmour <cameron.gilmour@doshii.io>
2023-04-25 18:37:59 +1000

    ready for dockerisation

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,17 +1,27 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template 
+import shutil
+import os
+import requests
+
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = 'static/'
 
 def get_most_recent_image():
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     if not files:
         return None
-    files = [f for f in files if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], f))]
+
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    files = [f for f in files if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], f)) and any(ext in f.lower() for ext in image_extensions)]
     if not files:
         return None
-    return os.path.join(app.config['UPLOAD_FOLDER'], max(files, key=os.path.getctime))
+    return os.path.join(app.config['UPLOAD_FOLDER'], max(files, key=lambda f: os.path.getctime(os.path.join(app.config['UPLOAD_FOLDER'], f))))
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,19 +30,28 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # check if the POST request has an image
-    if 'image' not in request.files:
-        return jsonify({'error': 'no image found'})
-    
-    # save the image to the uploads directory
-    image = request.files['image']
-    filename = image.filename
+    image_url = request.json.get('image_url')
+    print(image_url)
+
+    image_data = requests.get(image_url).content
+    filename = "image.jpg"  # Save the downloaded image as 'image.jpg'
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    image.save(filepath)
-    
-    # return the URL of the uploaded image
-    url = request.host_url + app.config['UPLOAD_FOLDER'] + '/' + filename
-    return jsonify({'url': url})
+
+    with open(filepath, 'wb') as output_file:
+        output_file.write(image_data)
+
+    response = requests.get(image_url, stream=True)
+
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            response.raw.decode_content = True
+            shutil.copyfileobj(response.raw, f)
+
+        url = request.host_url + app.config['UPLOAD_FOLDER'] + '/' + filename
+        return jsonify({'url': url}), 200
+    else:
+        return jsonify({'error': 'failed to download image'}), 500
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5069)
