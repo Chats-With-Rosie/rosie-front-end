26697b2fe5f969af24fb0b76ca55fc9e6dfc5cd0
Oliver <rowartu@gmail.com>
2023-04-26 15:37:57 +1000

    maybe fixed?

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,33 +1,28 @@
-from flask import Flask, request, jsonify, render_template
 import os
-import sqlite3
+import requests
+from gtts import gTTS
+from flask import Flask, request, jsonify, render_template
+from pydub import AudioSegment
+from pydub.playback import play
+from playsound import playsound
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-app.config['DATABASE'] = 'database.db'
-app.config['SECRET_KEY'] = 'secretkey'
-
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.execute('CREATE TABLE IF NOT EXISTS images (filename TEXT)')
-    conn.commit()
-    return conn
+app.config['UPLOAD_FOLDER'] = 'static'
 
 def get_most_recent_image():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT filename FROM images ORDER BY rowid DESC LIMIT 1')
-    result = cursor.fetchone()
-    if result is None:
-        return None
-    filename = result[0]
-    return os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-def save_image_to_db(filename):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO images VALUES (?)', (filename,))
-    conn.commit()
+    #FIX THIS TO DISPLAY IMAGE
+    # files = os.listdir(app.config['UPLOAD_FOLDER'])
+    # if not files:
+    #     return None
+    # files = [f for f in files if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], f))]
+    # if not files:
+    #     return None
+    # return os.path.join(app.config['UPLOAD_FOLDER'], max(files, key=os.path.getctime))
+    return "/static/uploads/aaa.png"
 
 @app.route('/', methods=['GET'])
 def index():
@@ -46,12 +41,48 @@ def upload():
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     image.save(filepath)
     
-    # save the filename to the database
-    save_image_to_db(filename)
-    
     # return the URL of the uploaded image
     url = request.host_url + app.config['UPLOAD_FOLDER'] + '/' + filename
     return jsonify({'url': url})
+def send_audio_to_endpoint(audio_filename, endpoint_url):
+    audio_filepath = os.path.abspath(audio_filename)
+
+    if not os.path.exists(audio_filepath):
+        print("Error: audio file not found.")
+        return None
+
+    with open(audio_filepath, 'rb') as audio_file:
+        files = {'file': (audio_filename, audio_file, 'audio/wav')}
+        response = requests.post(endpoint_url, files=files)
+
+    if response.status_code == 200:
+        print("Audio file sent successfully!")
+        print(f"Response content: {response.content}")
+        return response.json()["response"]
+    else:
+        print(f"Error sending audio file. Status code: {response.status_code}")
+        return None
+
+def text_to_speech(text, endpoint_url):
+    # Set language to English
+    language = 'en'
+
+    # Create gTTS object and save audio file to disk
+    tts = gTTS(text=text, lang=language)
+    tts.save("static/uploads/speak.wav")
+    
+@app.route('/speak', methods=['POST'])
+def think():
+    data_string = request.json.get('data')
+    endpoint_url = os.environ.get('FRONT_END_AUDIO_URL')
+    if data_string:
+        print(f"Received string: {data_string}")
+        response = data_string
+        text_to_speech(data_string, endpoint_url)
+        return jsonify({"response": response})
+    else:
+        return jsonify({"result": "error", "message": "No data received"}), 400
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7069)
+    app.run(debug=True)
