65b8f589d92ad89cd7d81fba539799d7a10d0b82
Cameron Gilmour <cameron.gilmour@doshii.io>
2023-04-27 14:13:09 +1000

    ready

--- Frontend/app.py
+++ Frontend/app.py
@@ -116,4 +116,4 @@ def text_to_speech():
     return jsonify({"response": "Audio file saved successfully"}), 200
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000)
